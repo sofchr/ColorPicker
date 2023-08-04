5a5c81a677de64ca2776f5399fbf9afadf82c987
Sofie Christensen <sofiec510@gmail.com>
2023-08-04 15:11:31 -0400

    first round of code without clicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 // Write your Color component here
+import { useState } from "react";
+// const [selectedColor, setSelectedColor] = useState("");
+
+const Color = ({ color }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
